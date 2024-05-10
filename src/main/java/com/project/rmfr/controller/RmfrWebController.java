4277a4f0fca1dc44d3c050781af49fd50ad9973c
seulgae <r1605866@naver.com>
2024-05-10 22:00:49 +0900

    [20240510]프론트 프록시 서버 구동 구현 프론트 프록시 서버를 띄워 개발하는 방법 REF: https://javabom.tistory.com/5
    1. package.json
    - 빌드명령어 추가
    "watch": "vue-cli-service build --watch" 내용 추가
    -> build --watch를 통해 수정이 일어날 시 실시간으로 재빌드 한다.
    
    2. OutputDir설정
      chainWebpack(config) { //빌드 시 빌드되어 나오는 js파일을 js폴더 아래로 묶어 빌드한다
        config.output.filename("js/[name].js");
      }...
    - 빌드 시 빌드되어 나오는 js파일을 js폴더 아래로 묶어 빌드한다.
    
    사용법 : 터미널 npm run watch 명령어 실행

--- src/main/java/com/project/rmfr/controller/RmfrWebController.java
+++ src/main/java/com/project/rmfr/controller/RmfrWebController.java
@@ -1,13 +1,17 @@
 package com.project.rmfr.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/")
 public class RmfrWebController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String rmfr() {
+        System.out.println("안녕!!!");
         return "index";
     }
+
 }
