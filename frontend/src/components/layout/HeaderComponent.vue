6d6be92c945ee63b89982d7e1765e4dbe06297bd
r1605866 <r1605866@naver.com>
2024-05-23 15:23:58 +0900

    [20240523]메인페이지 작업중. - b-navbar-nav 태그 길이조절 방법 찾아보기
    [작업순서]
    1. 사이드바 컴포넌트 만들기.
    2. 알람 컴포넌트 만들기.
    3. 유저 컴포넌트 만들기.

--- frontend/src/components/layout/HeaderComponent.vue
+++ frontend/src/components/layout/HeaderComponent.vue
@@ -2,7 +2,7 @@
   <div>
     <b-navbar toggleable="lg" type="" variant="info">
       <b-navbar-brand href="#">
-        <div id="menuToggle">
+        <div id="menubar">
           <span class="toggleBtn" @click="fn_toggleMenu">
             <font-awesome-icon :icon="['fas', 'bars']" />
           </span>
@@ -16,24 +16,24 @@
           <b-nav-item href="/">rmfr</b-nav-item>
         </b-navbar-nav>
 
+        <b-navbar-nav class="ms-auto" center>
+          <b-nav-form width="30%">
+            <b-form-input size="sm" class="mr-sm-2" placeholder="검색"></b-form-input>
+
+            <b-button size="sm" class="my-2 my-sm-0" type="submit" ><font-awesome-icon :icon="['fas', 'magnifying-glass']" /></b-button>
+          </b-nav-form>
+        </b-navbar-nav>
+
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ms-auto" right>
-          <b-nav-form>
-            <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-          </b-nav-form>
-
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item href="#">EN</b-dropdown-item>
-            <b-dropdown-item href="#">ES</b-dropdown-item>
-            <b-dropdown-item href="#">RU</b-dropdown-item>
-            <b-dropdown-item href="#">FA</b-dropdown-item>
-          </b-nav-item-dropdown>
+          <b-navbar-brand href="#">
+            <font-awesome-icon :icon="['far', 'bell']" />
+          </b-navbar-brand>
 
           <b-nav-item-dropdown right>
             <!-- Using 'button-content' slot -->
             <template #button-content>
-              <em>User</em>
+              <em><font-awesome-icon :icon="['far', 'user']" /></em>
             </template>
             <b-dropdown-item href="#">Profile</b-dropdown-item>
             <b-dropdown-item href="#">Sign Out</b-dropdown-item>
