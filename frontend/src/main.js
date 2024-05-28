e8a8d355b2d5184656999837f9ff2d99a48d2e1e
r1605 <r1605866@naver.com>
2024-05-28 17:44:27 +0900

    [20240528]회원가입 폼 작성중 - 이메일 인증 안되는 이슈 있는데 다른 환경에서 확인해보기. - 로그인 폼까지 완성해보기.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -14,11 +14,29 @@ import { far } from "@fortawesome/free-regular-svg-icons";
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
+// axios
+import axios from "axios";
+
 // fontawesome library : solid, brands, regular 타입 사용
 library.add(fas, fab, far);
 
 // application 객체 생성
 const app = createApp(App);
+
+// axios 기본 http header 값 셋팅 (get / post / put / delete)
+axios.defaults.headers.get["Content-Type"] = "application/json";
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.put["Content-Type"] = "application/json";
+axios.defaults.headers.delete["Content-Type"] = "application/json";
+
+// 전역변수 선언
+import { ref } from "vue";
+const menuDrawer = ref(false);
+app.config.globalProperties.axios = axios;
+app.config.globalProperties.$menuDrawer = menuDrawer;
+
+// fontawesome library : solid, brands, regular 타입 사용
+library.add(fas, fab, far);
 
 // application에 사용 가능하도록 설정
 app.use(routers)  // router 추가
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,16 +1,36 @@
 const { defineConfig } = require("@vue/cli-service");
+const path = require("path");
+
 module.exports = defineConfig({
   // outputDir: npm run build 로 빌드시 파일이 생성되는 위치입니다.
   outputDir: "../src/main/resources/static",
-  chainWebpack(config) { //빌드 시 빌드되어 나오는 js파일을 js폴더 아래로 묶어 빌드한다
+  chainWebpack: (config) => {
+    //빌드 시 빌드되어 나오는 js파일을 js폴더 아래로 묶어 빌드한다
     config.output.filename("js/[name].js");
+
+    //import 시 파일 파일 경로 alias 설정
+    config.resolve.alias.set(
+        "@b-js",
+        path.resolve(__dirname, "src/assets/js/")
+    );
+    config.resolve.alias.set(
+        "@b-css",
+        path.resolve(__dirname, "src/assets/css/")
+    );
+    config.resolve.alias.set(
+        "@b-components",
+        path.resolve(__dirname, "src/components/")
+    );
+    config.resolve.alias.set(
+        "@b-router",
+        path.resolve(__dirname, "src/router/")
+    );
   },
   indexPath: "index.html",
 
   // devServer : SpringBoot 의 내장 WAS 주소
-  devServer:{
-    proxy:  'http://localhost:8082',
-    disableHostCheck: true
-  }
-
+  devServer: {
+    proxy: "http://localhost:8082",
+    //disableHostCheck: true,
+  },
 });
