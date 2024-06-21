1e3e33e831f3e930adfe237dd597a1ce9a5197d8
jinwoong <rassayzsixt@gmail.com>
2024-06-21 11:20:50 +0900

    [240621] 이벤트 상세페이지 구현 중

---
+++ frontend/src/assets/vuetify/js/contents/main/mainMethods.js
@@ -0,0 +1,102 @@
+export default {
+  fnSetToday() {
+    var today = new Date();
+
+    var year = today.getFullYear();
+    var month = today.getMonth();
+    var date = today.getDate();
+
+    var bToday = this.today;
+
+    if (bToday != null) {
+      var bYear = bToday.getFullYear();
+      var bMonth = bToday.getMonth();
+      var bDate = bToday.getDate();
+
+      if (bYear == year && bMonth == month && bDate == date) {
+        return bToday;
+      }
+    }
+
+    this.today = new Date(year, month, date);
+    return new Date(year, month, date);
+  },
+  fnSetDate(diff) {
+    var days = ["s", "m", "w", "t", "t", "f", "s"];
+    var today = this.fnSetToday();
+
+    this.diff = this.diff + diff;
+
+    var midDate = new Date(
+      today.getFullYear(),
+      today.getMonth(),
+      today.getDate()
+    );
+
+    midDate.setDate(midDate.getDate() + this.diff);
+
+    for (var i in this.weekdays) {
+      var newDate = new Date(
+        midDate.getFullYear(),
+        midDate.getMonth(),
+        midDate.getDate()
+      );
+
+      newDate.setDate(newDate.getDate() + (i - 3));
+
+      var year = 0;
+      var month = 0;
+      var date = 0;
+      var day = 0;
+
+      year = newDate.getFullYear();
+      month = newDate.getMonth() + 1;
+      date = newDate.getDate();
+      day = newDate.getDay();
+      this.weekdays[i].realDate = new Date(year, month - 1, date).getTime();
+
+      this.weekdays[i].today =
+        this.today.getTime() == new Date(year, month - 1, date).getTime();
+      this.weekdays[i].name = days[newDate.getDay()];
+      this.weekdays[i].color = day == 6 ? "blue" : day == 0 ? "red" : "black";
+
+      this.weekdays[i].date =
+        year +
+        "-" +
+        (month > 9 ? month : "0" + month) +
+        "-" +
+        (date > 9 ? date : "0" + date);
+    }
+  },
+  fnSearchDate() {
+    if (this.searchDate != null) {
+      var year = this.searchDate.getFullYear();
+      var month = this.searchDate.getMonth();
+      var date = this.searchDate.getDate();
+
+      this.diff =
+        (new Date(year, month, date).getTime() - this.today.getTime()) /
+        (24 * 60 * 60 * 1000);
+
+      this.fnSetDate(0);
+    }
+  },
+  fnGoToday() {
+    this.diff = 0;
+    this.searchDate = null;
+    this.today = this.fnSetToday();
+    this.fnSetDate(0);
+  },
+  selectEvent(evt) {
+    for (var i in this.events) {
+      if (this.events[i] == evt) {
+        this.events[i].selected = true;
+      } else {
+        this.events[i].selected = false;
+      }
+    }
+  },
+  fnValue(v) {
+    console.log(v);
+  },
+};
--- frontend/src/components/vuetify/contents/main/MainView.vue
+++ frontend/src/components/vuetify/contents/main/MainView.vue
@@ -1,16 +1,33 @@
-<script setup></script>
+<script setup>
+import { VDateInput } from "vuetify/labs/VDateInput";
+import mainDatas from "@v-js/contents/main/mainDatas.js";
+import mainMethods from "@v-js/contents/main/mainMethods.js";
+</script>
 
 <template>
   <h3 class="card-header">
-    <v-icon icon="fas fa-e" size="22px"></v-icon>
-    <v-icon icon="fas fa-v" size="22px"></v-icon>
-    <v-icon icon="fas fa-e" size="22px"></v-icon>
-    <v-icon icon="fas fa-n" size="22px"></v-icon>
+    <v-icon icon="fas fa-m" size="22px"></v-icon>
+    <v-icon icon="fas fa-a" size="22px"></v-icon>
     <v-icon icon="fas fa-t" size="22px"></v-icon>
+    <v-icon icon="fas fa-c" size="22px"></v-icon>
+    <v-icon icon="fas fa-h" size="22px"></v-icon>
+    <v-icon icon="fas fa-e" size="22px"></v-icon>
     <v-icon icon="fas fa-s" size="22px"></v-icon>&nbsp;
     <v-btn icon="fas fa-chevron-right" size="22px"></v-btn>
   </h3>
   <v-card class="calendar-row">
+    <v-row>
+      <v-col cols="4">
+        <v-date-input
+          variant="solo"
+          prepend-icon=""
+          prepend-inner-icon="fa-magnifying-glass"
+          v-model="searchDate"
+          append-inner-icon="fa-clock-rotate-left"
+          @click:append-inner="fnGoToday"
+        ></v-date-input>
+      </v-col>
+    </v-row>
     <v-row>
       <v-col sm="1" class="event-btn-cols">
         <v-btn icon="fas fa-chevron-left" @click="fnSetDate(-7)"></v-btn>
@@ -44,15 +61,77 @@
               (weekday.today ? 'today' : '')
             "
           >
-            <v-chip
-              v-for="event in events.filter((e) => e.date === weekday.realDate)"
-              :key="event"
-              :prepend-icon="event.icon"
-              :color="event.color"
-              link
-            >
-              &nbsp;{{ event.type }} Match!
-            </v-chip>
+            <v-list bg-color="transparent">
+              <v-list-item
+                class="event-item"
+                v-for="event in events.filter(
+                  (e) => e.date === weekday.realDate
+                )"
+                :key="event"
+              >
+                <v-dialog
+                  v-model="event.selected"
+                  persistent
+                  transition="dialog-bottom-transition"
+                  full-width
+                >
+                  <template v-slot:activator="{ props: activatorProps }">
+                    <v-chip
+                      :prepend-icon="event.icon"
+                      :color="event.color"
+                      link
+                      v-bind="activatorProps"
+                    >
+                      &nbsp;{{ event.type }} Match!
+                    </v-chip>
+                  </template>
+
+                  <v-card>
+                    <v-toolbar>
+                      &nbsp;&nbsp;
+                      <v-icon :icon="event.icon"></v-icon>
+                      <v-toolbar-title>{{ event.type }}</v-toolbar-title>
+                      <v-spacer></v-spacer>
+
+                      <v-toolbar-items>
+                        <v-btn
+                          icon="fa-times"
+                          @click="event.selected = false"
+                          color="red"
+                        ></v-btn>
+                      </v-toolbar-items>
+                    </v-toolbar>
+
+                    <v-list lines="two" subheader>
+                      <v-list-subheader>Match Info</v-list-subheader>
+                      <v-row>
+                        <v-col cols="4">
+                          <v-list-item title="Participiant" link>
+                            <v-chip
+                              v-for="participiant in event.participiants"
+                              :key="participiant"
+                              link
+                              width="auto"
+                              >@{{ participiant }}
+                            </v-chip>
+                          </v-list-item>
+                          <v-list-item>
+                            <v-list-item title="Schedules" link>
+                              <v-date-picker
+                                class="dialogCalendar"
+                                v-model="event.realDate"
+                                @update:modelValue="fnValue"
+                              ></v-date-picker>
+                            </v-list-item>
+                          </v-list-item>
+                        </v-col>
+                        <v-col cols="8"></v-col>
+                      </v-row>
+                    </v-list>
+                  </v-card>
+                </v-dialog>
+              </v-list-item>
+            </v-list>
           </v-col>
         </v-row>
       </v-col>
@@ -68,187 +147,26 @@
 
 export default {
   data() {
-    return {
-      type: "",
-      today: null,
-      diff: 0,
-      weekdays: [
-        { name: "s", date: "", color: "red", today: false, realDate: null },
-        { name: "m", date: "", color: "black", today: false, realDate: null },
-        { name: "t", date: "", color: "black", today: false, realDate: null },
-        { name: "w", date: "", color: "black", today: false, realDate: null },
-        { name: "t", date: "", color: "black", today: false, realDate: null },
-        { name: "f", date: "", color: "black", today: false, realDate: null },
-        { name: "s", date: "", color: "blue", today: false, realDate: null },
-      ],
-      events: [
-        {
-          type: "diss",
-          genre: "hiphop",
-          icon: "fas fa-microphone-lines",
-          date: new Date(2024, 5, 22).getTime(),
-          color: "orange",
-        },
-        {
-          type: "box",
-          genre: "fight",
-          icon: "fas fa-user-ninja",
-          date: new Date(2024, 5, 17).getTime(),
-          color: "red",
-        },
-        {
-          type: "judo",
-          genre: "fight",
-          icon: "fas fa-user-ninja",
-          date: new Date(2024, 5, 19).getTime(),
-          color: "red",
-        },
-        {
-          type: "cycle",
-          genre: "race",
-          icon: "fas fa-person-biking",
-          date: new Date(2024, 5, 20).getTime(),
-          color: "primary",
-        },
-        {
-          type: "judo",
-          genre: "fight",
-          icon: "fas fa-user-ninja",
-          date: new Date(2024, 5, 22).getTime(),
-          color: "red",
-        },
-      ],
-    };
+    return mainDatas;
   },
   created() {
-    this.today = this.fnSetToday();
-    this.fnSetDate(0);
+    this.fnGoToday();
   },
-  methods: {
-    fnSetToday() {
-      var today = new Date();
-
-      var year = today.getFullYear();
-      var month = today.getMonth();
-      var date = today.getDate();
-
-      var bToday = this.today;
-
-      if (bToday != null) {
-        var bYear = bToday.getFullYear();
-        var bMonth = bToday.getMonth();
-        var bDate = bToday.getDate();
-
-        if (bYear == year && bMonth == month && bDate == date) {
-          return bToday;
-        }
-      }
-
-      this.today = new Date(year, month, date);
-      return new Date(year, month, date);
+  watch: {
+    searchDate() {
+      this.fnSearchDate();
     },
-    fnSetDate(diff) {
-      var days = ["s", "m", "w", "t", "t", "f", "s"];
-      var today = this.fnSetToday();
-
-      this.diff = this.diff + diff;
-
-      var midDate = new Date(
-        today.getFullYear(),
-        today.getMonth(),
-        today.getDate()
-      );
-
-      midDate.setDate(midDate.getDate() + this.diff);
-
-      for (var i in this.weekdays) {
-        var newDate = new Date(
-          midDate.getFullYear(),
-          midDate.getMonth(),
-          midDate.getDate()
-        );
-
-        newDate.setDate(newDate.getDate() + (i - 3));
-
-        var year = 0;
-        var month = 0;
-        var date = 0;
-        var day = 0;
-
-        year = newDate.getFullYear();
-        month = newDate.getMonth() + 1;
-        date = newDate.getDate();
-        day = newDate.getDay();
-        this.weekdays[i].realDate = new Date(year, month - 1, date).getTime();
-
-        this.weekdays[i].today =
-          this.today.getTime() == new Date(year, month - 1, date).getTime();
-        this.weekdays[i].name = days[newDate.getDay()];
-        this.weekdays[i].color = day == 6 ? "blue" : day == 0 ? "red" : "black";
-
-        this.weekdays[i].date =
-          year +
-          "-" +
-          (month > 9 ? month : "0" + month) +
-          "-" +
-          (date > 9 ? date : "0" + date);
-
-        console.log(this.weekdays[i].date);
-      }
+    events: {
+      handler(newValue, oldValue) {
+        console.log(newValue, oldValue);
+      },
     },
   },
+  methods: mainMethods,
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.card-header {
-  padding: 20px;
-  display: flex;
-  align-items: center;
-
-  button {
-    padding: 0;
-    font-size: 10px;
-  }
-}
-
-.today {
-  background-color: aliceblue;
-}
-
-.calendar-row {
-  margin: 10px;
-  padding: 30px;
-}
-
-.event-btn-cols {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.v-chip {
-  width: 95%;
-}
-
-.header-txt {
-  font-size: 15px;
-
-  hr {
-    margin: 8px;
-  }
-}
-
-.dateStr {
-  font-size: 18px;
-}
-
-.color-red {
-  color: red;
-}
-
-.color-blue {
-  color: blue;
-}
+@import "@v-css/contents/main/main.css";
 </style>
